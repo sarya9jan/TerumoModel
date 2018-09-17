e99c9a525a1f890aaacf96cf1accfb630ae0dc87
SS00576773 <suryashrivastava92@gmail.com>
2018-09-17 18:54:07 +0530

    changed

--- app/src/main/java/com/example/ss00576773/terumomodel/MainActivity.java
+++ app/src/main/java/com/example/ss00576773/terumomodel/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        Toast.make(this,"Hello",Toast.LengthLong).show();
+        //Toast.make(this,"Hello",Toast.LengthLong).show();
     }
 }
